0e9a81c05a7a21ef9f9aeb01c39b6a902c743587
Momoko OSHIRO <e205430@ie.u-ryukyu.ac.jp>
2021-12-21 18:55:26 +0900

    2nd commit

--- src/jp/ac/uryukyu/ie/e205430/report5.java
+++ src/jp/ac/uryukyu/ie/e205430/report5.java
@@ -2,7 +2,14 @@ package jp.ac.uryukyu.ie.e205430;
 
 public class report5 {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try {
+            //処理したいコード
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            //Exceptionが起きたときの処理
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        } 
     }
 }
